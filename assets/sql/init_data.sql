8e4e982eba8a636a546f26534eaef2ba007889d1
fhenzin7 <faustohenrique7645@gmail.com>
2021-11-18 17:44:29 +0000

    alteração

--- assets/sql/init_data.sql
+++ assets/sql/init_data.sql
@@ -1,6 +1,6 @@
 -- Código SQL com a inclusão dos registros iniciais do banco de dados
 
-INSERT INTO filmes (titulos) VALUES
+INSERT INTO filmes (titulo) VALUES
 ('Entre Facas e Segredos'),
 ('A Sociedade do Anel'),
 ('O Hobbit');
